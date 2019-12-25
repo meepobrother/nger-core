e6b5cc022b1e4d71be2541e0cd96b1529b48aa1f
m18317252500 <m18317252500_1@163.com>
2019-12-25 17:20:51 +0800

    rm store decorator

--- packages/core/lib/decorator/store.ts
+++
@@ -1,9 +0,0 @@
-import { createClassDecorator, createMethodDecorator, Type } from "@nger/decorator";
-export const ReducerMetadataKey = `ReducerMetadataKey`;
-export interface ReducerOptions {
-    name: string;
-    store: any;
-}
-export const Reducer = createClassDecorator<ReducerOptions>(ReducerMetadataKey);
-export const CaseMetadataKey = `CaseMetadataKey`;
-export const Case = createMethodDecorator<any>(CaseMetadataKey);
